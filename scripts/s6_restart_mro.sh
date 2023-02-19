569cf0effbf02a78612134f0195c80d282781114
Wei Liu <liuwei_berkeley@berkeley.edu>
2023-02-18 21:36:33 -0800

    set eth ports for pol0/1 dynamically.

--- scripts/s6_restart_mro.sh
+++ scripts/s6_restart_mro.sh
@@ -6,6 +6,18 @@ export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 # get the data path
 # if it's not defined, the default path will be /data01/serendip6_data
 DATA_DIR=$S6_DATA_DIR
+POL0E=$POL0_ETH
+POL1E=$POL1_ETH
+
+# set default values
+if [ ! ${POL0E} ]; then 
+    POL0E="enp216s0f0"
+fi
+
+if [ ! ${POL1E} ]; then 
+    POL0E="enp216s0f1"
+fi
+
 if [ ! ${DATA_DIR} ]; then 
     DATA_DIR=/data01/serendip6_data
 fi
@@ -20,8 +32,11 @@ else
     exit 1
 fi
 
+echo "Pol0 Eth: "$POL0E
+echo "Pol1 Eth: "$POL1E
+
 # Ready to go!
-cd ${DATA_DIR} ; pkill -f "hashpipe -p serendip6" ; /usr/local/bin/s6_init_mro.sh
+cd ${DATA_DIR} ; pkill -f "hashpipe -p serendip6" ; /usr/local/bin/s6_init_mro.sh $POL0E $POL1E
 hashpipe_check_status -k RUNALWYS -I 0 -s 1
 hashpipe_check_status -k RUNALWYS -I 1 -s 1
 hashpipe_check_status -k IDLE     -I 0 -s 0
